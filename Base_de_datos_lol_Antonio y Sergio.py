e5ca9ea50649faf4e993cafbabd77311ffbcd2ec
Aforlop <aforlop2209@g.educaand.es>
2025-02-14 11:06:51 +0100

    Se añade un archivo .db y actualización del código
    Se añade un archivo .db con algunos datos ya añadidos y también, se añaden las funciones para listar las regiones y líneas, y las funciones para eliminar tanto regiones como lineas, y también las funciones para actualizar tanto líneas como regiones.

--- Base_de_datos_lol_Antonio y Sergio.py
+++ Base_de_datos_lol_Antonio y Sergio.py
@@ -59,6 +59,24 @@ def listar_personajes():
         print(p)
     conexion.close()
 
+def listar_regiones():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM Regiones")
+    regiones = cursor.fetchall()
+    for r in regiones:
+        print(r)
+    conexion.close()
+
+def listar_lineas():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM Lineas")
+    lineas = cursor.fetchall()
+    for l in lineas:
+        print(l)
+    conexion.close()
+
 def actualizar_personaje(id_personaje, nuevo_nombre, nuevo_anyo, nueva_fuente, nueva_region, nueva_linea):
     conexion = conectar()
     cursor = conexion.cursor()
@@ -67,10 +85,38 @@ def actualizar_personaje(id_personaje, nuevo_nombre, nuevo_anyo, nueva_fuente, n
     conexion.commit()
     conexion.close()
 
+def actualizar_region(id_region, nuevo_nombre):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE Regiones SET nombre_region=? WHERE id_region=?", (nuevo_nombre, id_region))
+    conexion.commit()
+    conexion.close()
+
+def actualizar_linea(id_linea, nuevo_nombre):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE Lineas SET nombre_linea=? WHERE id_linea=?", (nuevo_nombre, id_linea))
+    conexion.commit()
+    conexion.close()
+
 def eliminar_personaje(id_personaje):
     conexion = conectar()
     cursor = conexion.cursor()
     cursor.execute("DELETE FROM Personajes WHERE id_personaje=?", (id_personaje,))
+    conexion.commit()
+    conexion.close()
+
+def eliminar_region(id_region):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM Regiones WHERE id_region=?", (id_region,))
+    conexion.commit()
+    conexion.close()
+
+def eliminar_linea(id_linea):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM Lineas WHERE id_linea=?", (id_linea,))
     conexion.commit()
     conexion.close()
 
@@ -83,9 +129,15 @@ def menu():
         print("2. Agregar Línea")
         print("3. Agregar Personaje")
         print("4. Listar Personajes")
-        print("5. Actualizar Personaje")
-        print("6. Eliminar Personaje")
-        print("7. Salir")
+        print("5. Listar Regiones")
+        print("6. Listar Líneas")
+        print("7. Actualizar Personaje")
+        print("8. Actualizar Región")
+        print("9. Actualizar Línea")
+        print("10. Eliminar Personaje")
+        print("11. Eliminar Región")
+        print("12. Eliminar Línea")
+        print("13. Salir")
         opcion = input("Seleccione una opción: ")
         
         if opcion == "1":
@@ -104,6 +156,10 @@ def menu():
         elif opcion == "4":
             listar_personajes()
         elif opcion == "5":
+            listar_regiones()
+        elif opcion == "6":
+            listar_lineas()
+        elif opcion == "7":
             id_p = int(input("ID del personaje a actualizar: "))
             nombre = input("Nuevo nombre: ")
             anyo = int(input("Nuevo año de salida: "))
@@ -111,10 +167,24 @@ def menu():
             id_region = int(input("Nuevo ID de región: "))
             id_linea = int(input("Nuevo ID de línea: "))
             actualizar_personaje(id_p, nombre, anyo, poder, id_region, id_linea)
-        elif opcion == "6":
+        elif opcion == "8":
+            id_r = int(input("ID de la región a actualizar: "))
+            nuevo_nombre = input("Nuevo nombre de la región: ")
+            actualizar_region(id_r, nuevo_nombre)
+        elif opcion == "9":
+            id_l = int(input("ID de la línea a actualizar: "))
+            nuevo_nombre = input("Nuevo nombre de la línea: ")
+            actualizar_linea(id_l, nuevo_nombre)
+        elif opcion == "10":
             id_p = int(input("ID del personaje a eliminar: "))
             eliminar_personaje(id_p)
-        elif opcion == "7":
+        elif opcion == "11":
+            id_r = int(input("ID de la región a eliminar: "))
+            eliminar_region(id_r)
+        elif opcion == "12":
+            id_l = int(input("ID de la línea a eliminar: "))
+            eliminar_linea(id_l)
+        elif opcion == "13":
             continuar = False
         else:
             print("Opción no válida.")
